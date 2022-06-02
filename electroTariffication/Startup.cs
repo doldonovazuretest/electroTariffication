b7fdc18b91d2802b31f9697dd5814340c5233a0b
Yan Doldonov <yan.doldonov@crucel.net>
2022-06-02 22:31:39 +0200

    add startup class to make swagger template happy

---
+++ electroTariffication/Startup.cs
@@ -0,0 +1,43 @@
+﻿namespace electroTariffication
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
+        {
+            Configuration = configuration;
+            Environment = environment;
+        }
+
+        public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Environment { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();           
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            services.AddMemoryCache();
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+
+            // Configure the HTTP request pipeline.
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });   
+        }
+    }
+}
